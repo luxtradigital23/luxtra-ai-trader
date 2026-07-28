36a9ec069223dfb20f95a774cd5ecb697b75240d
Luxtra Digital <lojasejo24@gmail.com>
2026-07-28 16:17:57 -0400

    Remove misplaced B3 Trading Supabase configuration

--- config.js
+++
@@ -1,5 +0,0 @@
-window.B3_CONFIG = {
-  supabaseUrl: "https://cidfriemyurdnupgjjug.supabase.co",
-  supabaseAnonKey: "sb_publishable_GJje6QBkIdr_90bGBiB2aw_uK96KmvZ",
-  liveAccountsEnabled: false,
-};
